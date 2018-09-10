bd081798cac558e22f19dd3619482c48ce62d52a
valdemiro neto <valdemiroads@gmail.com>
2018-09-10 20:00:38 -0300

    projeto disciplina topicos especiais

---
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/topicos/ProjetoTopicosEspeciaisApplication.java
@@ -0,0 +1,12 @@
+package fvs.edu.topicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTopicosEspeciaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTopicosEspeciaisApplication.class, args);
+	}
+}
