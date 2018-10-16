6271c6e3ece3459f1b1641cb97343188f499cbef
valdemiroads@gmail.com <valdemiroads@gmail.com>
2018-10-16 19:49:05 -0300

    inicio do dominio Pessoa

---
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/enums/TipoCliente.java
@@ -0,0 +1,18 @@
+package fvs.edu.br.topicos.enums;
+
+public enum TipoCliente {
+
+	PESSOAFISICA(1),
+	PESSOAJURIDICA(2);
+	
+	int tipoPessoa;
+	
+	TipoCliente(int tipoPessoa){
+		this.tipoPessoa=tipoPessoa;
+	}
+	
+	
+	
+	
+	
+}
